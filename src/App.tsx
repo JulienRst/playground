e21b19bdcb1afc87c90c695c694bb473906131af
julien-rousset <julien.rousset@matters.tech>
2023-03-11 12:06:40 +0100

    feat(seo): Add favicons and manifests

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,26 @@ import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 
 const App: React.FC = () => {
+  console.log(
+    '%cJulien Rousset | PlayGround',
+    'padding: 3px; background-color: white; color: #101010',
+  );
+  console.log(
+    '%cMail          %cjulien.rousset01@gmail.com',
+    'padding: 3px; background-color: white; color: #101010',
+    ' padding: 3px; background-color: white; color: black',
+  );
+  console.log(
+    '%cPhone         %c+33608957135',
+    'padding: 3px; background-color: white; color: #101010',
+    ' padding: 3px; background-color: white; color: black',
+  );
+  console.log(
+    '%cTwitter       %c@JulienRst',
+    'padding: 3px; background-color: white; color: #101010',
+    ' padding: 3px; background-color: white; color: black',
+  );
+
   return <RouterProvider router={router} />;
 };
 
